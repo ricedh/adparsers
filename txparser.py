86c3823bc36cc95eb576729c327c4b966e5c5c4a
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-03-23 15:34:32 -0500

    add lon, lat to filename to prevent duplicate filename for different ads, import os

--- txparser.py
+++ txparser.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
+import os
 import csv
 import zipfile
-from urlparse import urlparse
+from urlparse import urlparse, parse_qs
 
 def parse_url(permalink):
     """ Take a permalink from the runaway ads spreadsheet, return list of
-    newspaper ID, unique metapth ID for issues, and page for later recomposition. 
+    newspaper ID, unique metapth ID for issues, and page for recomposition. 
     URLs take the form, http://texashistory.unt.edu/ark:/67531/metapth78134/m1/3/
     zoom/?zoom=6&lat=851.5&lon=996.5
     """
     parsed_url = urlparse(permalink)
     paths = parsed_url.path.split('/')[2:6]
-    # queries = parse_qs(parsed_url.query)
-    # lat, lon = queries['lat'][0], queries['lon'][0]
-    return paths
+    queries = parse_qs(parsed_url.query)
+    lat, lon = queries['lat'], queries['lon']
+    urlparts = paths + lat + lon
+    return urlparts
 
 def get_transcriptions_from_csv(fileobj):
     """ Take an opened CSV downloaded from Google Docs, return list of
@@ -29,20 +31,20 @@ def get_transcriptions_from_csv(fileobj):
         if 'TRANSCRIPTION' in cleaned_row: transcribed.append(cleaned_row)
     return transcribed
 
-f = open('/Users/wcm1/Inbox/telegraph-1838.csv', 'rb')
-z = zipfile.ZipFile('/Users/wcm1/Desktop/1838.zip', 'w')
-transcribed_ads = get_transcriptions_from_csv(f)
+path = '/Users/wcm1/Inbox/csv'
+z = zipfile.ZipFile('/Users/wcm1/Desktop/1836-1840.zip', 'a')
 
-print len(transcribed_ads)
-
-for ad in transcribed_ads:
-    for key in ad:
-        if len(ad[key]) < 2: ad[key] = '0' + ad[key]
-    paths = parse_url(ad['PERMALINK'])
-    new_file_name = 'TX_' + ad['YYYY'] + ad['MM'] + ad['DD'] + '_Telegraph_' + '-'.join(paths) + '.txt'
-    z.writestr(new_file_name, ad['TRANSCRIPTION'])
+for file in os.listdir(path):
+    if file.endswith('.csv'):
+        f = open(path + '/' + file, 'rb')
+        transcribed_ads = get_transcriptions_from_csv(f)
+        for ad in transcribed_ads:
+            for key in ad:
+                if len(ad[key]) < 2: ad[key] = '0' + ad[key]
+            paths = parse_url(ad['PERMALINK'])
+            new_file_name = 'TX_' + ad['YYYY'] + ad['MM'] + ad['DD'] + '_Telegraph_' + '-'.join(paths) + '.txt'
+            z.writestr(new_file_name, ad['TRANSCRIPTION'])
+        f.close()
 
 z.close()
-f.close()
-
 
