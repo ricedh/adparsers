ac0c7b777faef7ed8e731e8ea4f2e5abe55a739a
FBettati <franco.d.bettati@gmail.com>
2014-04-21 15:39:50 -0500

    Create tf-idf_runnable.py

---
+++ tf-idf_runnable.py
@@ -0,0 +1,44 @@
+import sys, os, tf_idf
+
+ts = tf_idf.tfScore();
+print("Welcome to the TF-IDF scorer. Input HELP for a list of functions.")
+
+while True:
+
+	var = raw_input("Please input command here.\n");
+	var = var.split();
+
+	# Help documentation.
+	if ('HELP' in var):
+		print("Supported functions include:\n - ADD_DOCUMENT(inDoc)");
+
+	elif ('ADD_DIRECTORY' in var):
+		# If the input argument is a directory, parse each file into the score object.
+		if (os.path.isdir(sys.argv[1]) or os.path.isdir(os.getcwd()+'/'+sys.argv[1])):
+			for filename in os.listdir(os.getcwd()):
+				if filename.endswith(".txt"):
+					list_of_words = [];
+					with open(filename, 'r') as open_file:
+						file_text = open_file.readlines();
+						for line in file_text:
+							words = line.split();
+							list_of_words.extend(words);
+					ts.addDocument(filename[:-4], list_of_words);
+		else:
+			print("Input directory not found.\n");
+
+	elif ('ADD_DOCUMENT' in var):
+		with open(var[1], 'r') as open_file:
+			file_text = open_file.readlines();
+			for line in file_text:
+				words = line.split();
+				list_of_words.extend(words);
+			ts.addDocument(var[1][:-4], list_of_words);
+
+	elif ('TF-IDF' in var):
+		with open(var[1], 'r') as open_file:
+			file_text = open_file.readlines();
+			for line in file_text:
+				words = line.split();
+				list_of_words.extend(words);
+			ts.addDocument(var[1][:-4], list_of_words);
